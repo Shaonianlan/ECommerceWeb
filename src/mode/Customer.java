5afd16f10f142df6cbfd84862b198afcfb110354
1009647526@qq.com <1009647526@qq.com>
2018-01-22 12:48:38 +0800

    创建对象模型

---
+++ src/mode/Customer.java
@@ -0,0 +1,72 @@
+package mode;
+
+public class Customer {
+  private String user_name;
+  private String user_password;
+  private String user_sex;
+  private String user_phone;
+  private String user_email;
+  private String user_createtime;
+  private String user_address;
+  private String user_id;
+
+  public String getUser_id(){return user_id;}
+
+  public void setUser_id(String user_id){this.user_id = user_id;}
+
+  public String getUser_name() {
+    return user_name;
+  }
+
+  public void setUser_name(String user_name) {
+    this.user_name = user_name;
+  }
+
+  public String getUser_password() {
+    return user_password;
+  }
+
+  public void setUser_password(String user_password) {
+    this.user_password = user_password;
+  }
+
+  public String getUser_sex() {
+    return user_sex;
+  }
+
+  public void setUser_sex(String user_sex) {
+    this.user_sex = user_sex;
+  }
+
+  public String getUser_phone() {
+    return user_phone;
+  }
+
+  public void setUser_phone(String user_phone) {
+    this.user_phone = user_phone;
+  }
+
+  public String getUser_email() {
+    return user_email;
+  }
+
+  public void setUser_email(String user_email) {
+    this.user_email = user_email;
+  }
+
+  public String getUser_createtime() {
+    return user_createtime;
+  }
+
+  public void setUser_createtime(String user_createtime) {
+    this.user_createtime = user_createtime;
+  }
+
+  public String getUser_address() {
+    return user_address;
+  }
+
+  public void setUser_address(String user_address) {
+    this.user_address = user_address;
+  }
+}
